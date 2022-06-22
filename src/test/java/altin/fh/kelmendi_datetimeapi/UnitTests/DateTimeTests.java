f2d4fd8a0a7aba73b57e07f725c55cc308699fd8
ALTINLAPTOP\User <wi21b054@technikum-wien.at>
2022-06-22 20:16:56 +0200

    feature Time und UnitTest for Time implemented!

--- src/test/java/altin/fh/kelmendi_datetimeapi/UnitTests/DateTimeTests.java
+++ src/test/java/altin/fh/kelmendi_datetimeapi/UnitTests/DateTimeTests.java
@@ -4,6 +4,7 @@ import altin.fh.kelmendi_datetimeapi.controller.DateTimeController;
 import org.junit.*;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class DateTimeTests {
 
@@ -13,6 +14,16 @@ public class DateTimeTests {
 
         String actual = controller.today();
         String expected = LocalDate.now().toString();
+
+        Assert.assertEquals("Should be today!",expected, actual);
+    }
+
+    @Test
+    public void testTime(){
+        DateTimeController controller = new DateTimeController();
+
+        String actual = controller.now().substring(0,6);
+        String expected = LocalTime.now().toString().substring(0,6);
 
         Assert.assertEquals("Should be today!",expected, actual);
     }

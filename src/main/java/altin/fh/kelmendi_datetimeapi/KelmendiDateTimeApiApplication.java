33b212dab56e3a50750d5055b58e04b6724f7283
ALTINLAPTOP\User <wi21b054@technikum-wien.at>
2022-06-22 19:49:18 +0200

    Initial commit

---
+++ src/main/java/altin/fh/kelmendi_datetimeapi/KelmendiDateTimeApiApplication.java
@@ -0,0 +1,13 @@
+package altin.fh.kelmendi_datetimeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelmendiDateTimeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KelmendiDateTimeApiApplication.class, args);
+    }
+
+}

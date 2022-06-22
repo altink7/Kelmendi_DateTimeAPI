f0b5b86c71870781e8c47c1cd42019b47ac0dd80
ALTINLAPTOP\User <wi21b054@technikum-wien.at>
2022-06-22 20:36:30 +0200

    feature Time und UnitTest for Time implemented!

--- src/main/java/altin/fh/kelmendi_datetimeapi/controller/DateTimeController.java
+++ src/main/java/altin/fh/kelmendi_datetimeapi/controller/DateTimeController.java
@@ -2,6 +2,7 @@ package altin.fh.kelmendi_datetimeapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -11,16 +12,26 @@ import java.time.LocalTime;
 @RequestMapping("api")
 public class DateTimeController {
 
-    @GetMapping("today")
+    @GetMapping("/today")
     public String today(){
         LocalDate today = LocalDate.now();
         return today.toString();
     }
 
-    @GetMapping("now")
+    @GetMapping("/now")
     public String now(){
         LocalTime now = LocalTime.now();
-        return now.toString();
+        return now.toString().substring(0,8);
+    }
+
+    @GetMapping("/now/f")
+    public String nowFormat(@RequestParam String format){
+        LocalTime now = LocalTime.now();
+        if(format.matches("HH:mm")){
+            return now.toString().substring(0,5);
+        }else{
+            return now.toString().substring(0,8);
+        }
     }
 
 }
